5e47e6b559a49892caa87015a91cbba2127acab9
artyom-tugushev <152002484+artyom-tugushev@users.noreply.github.com>
2024-09-16 14:53:23 +0300

    Добавлен весь проект

--- src/App.vue
+++ src/App.vue
@@ -1,118 +1,136 @@
 <template>
-  <div class="container">
-    <div class="card flex justify-content-center mt-5">
-      <p-dropdown
-        v-model="selectedFilter"
-        :options="filters"
-        placeholder="Sort"
-        class="w-full border-2 shadow-1 md:w-14rem"
+  <main-menu></main-menu>
+  <!-- хэдер -->
+  <div class="header align-items-center flex my-4">
+    <div class="dm-sans-700 text-3xl">Vehichles</div>
+    <p-button
+      class="ml-3"
+      :label="data.length.toString()"
+      severity="secondary"
+    />
+    <div class="ml-auto flex align-items-center mr-4">
+      <p-button
+        class="header-button mr-4 red-border"
+        icon="pi pi-plus"
+        severity="danger"
+        outlined
+        aria-label="Cancel"
       />
-    </div>
-
-    <div class="grid mt-3">
-      <div
-        class="col-12 sm:col-6 lg:col-4"
-        v-for="car of sortedData"
-        :key="car.id"
-      >
-        <div
-          class="flex flex-column p-5 surface-0 border-2 border-300 border-round-lg gap-2"
-        >
-          <div class="flex gap-2 align-items-center">
-            <div class="flex flex-wrap gap-1">
-              <div
-                @click="openNameModal(car.name, car.id)"
-                class="text-xl text-secondary font-semibold cursor-pointer"
-              >
-                {{ car.name }}
-              </div>
-              <div
-                @click="openModelModal(car.model, car.id)"
-                class="text-xl text-primary font-semibold cursor-pointer"
-              >
-                {{ car.model }}
-              </div>
-            </div>
-            <div class="text-lg ml-auto">{{ car.year }}</div>
-          </div>
-
-          <div
-            @click="openPriceModal(car.price, car.id)"
-            class="text-xl text-secondary font-semibold cursor-pointer"
-          >
-            {{ `$ ${car.price.toLocaleString("ru-RU")}` }}
-          </div>
-
-          <div class="flex gap-2 align-items-center">
-            <div
-              class="border-round color-icon"
-              :class="[
-                car.color === 'black' ? 'surface-900' : '',
-                car.color === 'silver' ? 'surface-300' : `bg-${car.color}-500`,
-              ]"
-            ></div>
-            <div>
-              {{ car.color }}
-            </div>
-          </div>
-
-          <div class="mt-2">
-            <GoogleMap
-              api-key="AIzaSyB7bEXreK2RKXtXbK0SWchgy7CLqCbzGhY"
-              style="width: 100%; height: 300px"
-              :zoom="12"
-              :center="{ lat: car.latitude, lng: car.longitude }"
-              ><Marker
-                :options="{
-                  position: { lat: car.latitude, lng: car.longitude },
-                }"
-              />
-            </GoogleMap>
-          </div>
-        </div>
+      <div class="flex align-items-center">
+        <img
+          class="profile-pic mr-2"
+          src="./assets/images/user_photo.jpg"
+          alt=""
+        />
+        <div class="dm-sans-500">John Doe</div>
       </div>
     </div>
 
-    <div class="grid my-6">
-      <div
-        class="col-12 sm:col-6 sm:col-offset-3 lg:col-4 lg:col-offset-4 py-0"
-      >
-        <div class="text-300 text-center mb-3">
-          Test assignment from
-          <a
-            class="text-300 text-center mb-3"
-            target="_blank"
-            href="https://t.me/artyomtugushev"
-            >Artyom Tugushev</a
-          >
-        </div>
-      </div>
-    </div>
+    <p-dropdown
+      v-model="selectedLang"
+      :options="languages"
+      optionLabel="lang"
+      placeholder="Select a City"
+      class="max-w-6rem md:w-14rem"
+    />
   </div>
 
-  <change-name-modal
-    ref="changeNameModal"
-    :car="selectedCar"
-    @updateName="onUpdateName"
-  >
-    ></change-name-modal
-  >
+  <div class="border-05 border-300 my-2"></div>
 
-  <change-name-modal
-    ref="changeModelModal"
-    :car="selectedCar"
-    @updateName="onUpdateModel"
-  >
-    ></change-name-modal
-  >
+  <!-- серчбар и дропдаун для пагинатора -->
+  <div class="container default-layout">
+    <div class="grid mt-4">
+      <div class="col-12 sm:col-6 lg:col-4">
+        <p-inputtext
+          type="number"
+          class="w-full"
+          placeholder="Search VIN"
+          v-model="vinSearch"
+        >
+        </p-inputtext>
+      </div>
+      <div class="col-12 sm:col-6 lg:col-4">
+        <div class="flex">
+          <div class="dm-sans-500 mr-1">Select vehicles per page:</div>
+          <p-dropdown
+            v-model="rowsPerPage"
+            :options="rowsOptions"
+            class="max-w-6rem md:w-14rem"
+          />
+        </div>
+      </div>
+      <div class="col-12 sm:col-6 lg:col-4 flex">
+        <p-button
+          label="ADD VEHICLE"
+          class="ml-auto red-button"
+          icon="pi pi-plus"
+          severity="danger"
+          aria-label="Cancel"
+        />
+      </div>
 
-  <change-name-modal
-    ref="changePriceModal"
-    :car="selectedCar"
-    @updateName="onUpdatePrice"
-  >
-    ></change-name-modal
-  >
+      <!-- отображение карточек с машинами -->
+      <div
+        class="mt-4 col-12 sm:col-6 lg:col-4"
+        v-for="car in paginatedCars"
+        :key="car.id"
+      >
+        <div class="p-3 surface-100 border-round-lg">
+          <div class="flex mb-1">
+            <img
+              class="ml-auto"
+              src="./assets/images/more_horizontal.svg"
+              alt=""
+            />
+          </div>
+          <img class="car-preview" :src="car.placeholder" alt="" />
+
+          <div class="dm-sans-700 mt-2">
+            {{
+              car.vehicle_name && car.vehicle_name !== "undefined undefined"
+                ? car.vehicle_name.toUpperCase()
+                : "Unknown Car".toUpperCase()
+            }}
+          </div>
+          <div v-if="car.vin == +car.vin" class="dm-sans-500 text-500 mt-2">
+            VIN
+            {{ car.vin }}
+          </div>
+          <div class="border-05 border-300 my-3"></div>
+          <div class="flex align-items-center dm-sans-500">
+            <div
+              class="inline-block p-1 border-round-sm surface-200"
+              :class="{ checked: +car.angles_count === 16 }"
+            >
+              <div class="flex gap-1">
+                <img
+                  v-if="+car.angles_count === 16"
+                  class="pl-1"
+                  src="./assets/images/check.svg"
+                  alt=""
+                />
+                <div class="px-1">{{ car.angles_count }}/16</div>
+              </div>
+            </div>
+            <div class="ml-auto text-500">3 days left</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex align-items-center mt-3 mb-5">
+      <div class="dm-sans-500">
+        Showing {{ filteredData.length }} out of {{ data.length }}
+      </div>
+
+      <p-paginator
+        class="ml-auto"
+        :rows="rowsPerPage"
+        :totalRecords="filteredData.length"
+        @page="onPageChange"
+      >
+      </p-paginator>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -122,9 +140,9 @@ import Dialog from "primevue/dialog";
 import InputText from "primevue/inputtext";
 import InputNumber from "primevue/inputnumber";
 import Button from "primevue/button";
-import { GoogleMap, Marker } from "vue3-google-map";
+import Paginator from "primevue/paginator";
 
-import ChangeNameModal from "./components/ChangeNameModal.vue";
+import MainMenu from "./components/MainMenu.vue";
 
 export default {
   components: {
@@ -133,88 +151,99 @@ export default {
     "p-button": Button,
     "p-inputtext": InputText,
     "p-inputnumber": InputNumber,
-    GoogleMap,
-    Marker,
-    ChangeNameModal,
+    "p-paginator": Paginator,
+    MainMenu,
   },
   data() {
     return {
-      search: "",
       data: [],
-      filters: ["price ↑", "price ↓", "year", "no filter"],
-      selectedFilter: null,
+      filteredData: [],
+      vinSearch: null,
 
-      selectedCar: { carName: null, carId: null },
+      rowsPerPage: 9,
+      currentPage: 1,
+      rowsOptions: [9, 20],
+
+      selectedLang: { lang: "En", code: "eng" },
+      languages: [
+        { lang: "En", code: "eng" },
+        { lang: "Ru", code: "rus" },
+      ],
     };
   },
   computed: {
-    sortedData() {
-      if (this.selectedFilter === "year") {
-        return this.data.sort((a, b) => b.year - a.year);
-      } else if (this.selectedFilter === "price ↑") {
-        return this.data.sort((a, b) => a.price - b.price);
-      } else if (this.selectedFilter === "price ↓") {
-        return this.data.sort((a, b) => b.price - a.price);
-      } else if (!this.selectedFilter || this.selectedFilter === "no filter") {
-        return this.data.sort((a, b) => b.id - a.id);
+    paginatedCars() {
+      const start = (this.currentPage - 1) * this.rowsPerPage;
+      const end = start + this.rowsPerPage;
+      return this.filteredData.slice(start, end);
+    },
+  },
+  watch: {
+    vinSearch() {
+      this.filterData();
+    },
+    rowsPerPage() {
+      this.currentPage = 1;
+    },
+  },
+  methods: {
+    async getData() {
+      try {
+        const response = await axios.get(
+          "https://api.caiman-app.de/api/cars-test"
+        );
+        this.data = response.data.data;
+        this.filteredData = this.data;
+      } catch (error) {
+        console.error(error);
       }
+    },
+    filterData() {
+      if (!this.vinSearch) {
+        return (this.filteredData = this.data);
+      }
+      return (this.filteredData = this.data.filter((car) =>
+        car.vin.toString().includes(this.vinSearch.toString())
+      ));
+    },
+    onPageChange(event) {
+      this.currentPage = event.page + 1;
     },
   },
   mounted() {
     this.getData();
   },
-  methods: {
-    openNameModal(name, id) {
-      this.selectedCar.carName = name;
-      this.selectedCar.carId = id;
-      this.$refs.changeNameModal.openModal();
-    },
-    openModelModal(name, id) {
-      this.selectedCar.carName = name;
-      this.selectedCar.carId = id;
-      this.$refs.changeModelModal.openModal();
-    },
-    openPriceModal(name, id) {
-      this.selectedCar.carName = name;
-      this.selectedCar.carId = id;
-      this.$refs.changePriceModal.openModal();
-    },
-
-    onUpdateName(data) {
-      const car = this.data.find((item) => item.id === data.id);
-      car.name = data.name;
-    },
-    onUpdateModel(data) {
-      const car = this.data.find((item) => item.id === data.id);
-      car.model = data.name;
-    },
-    onUpdatePrice(data) {
-      const car = this.data.find((item) => item.id === data.id);
-      car.price = data.name;
-    },
-
-    deleteCar(id) {
-      this.data = this.data.filter((item) => item.id !== id);
-    },
-
-    async getData() {
-      try {
-        const response = await axios.get(
-          "https://test.tspb.su/test-task/vehicles"
-        );
-        this.data = response.data;
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  },
 };
 </script>
 
 <style lang="scss" scoped>
-.color-icon {
-  border: 2px solid #dfdfdf;
-  height: 24px;
-  width: 24px;
+.header {
+  margin-left: 288px;
+  margin-right: 32px;
+}
+.header-button {
+  width: 42px;
+  height: 42px;
+}
+.profile-pic {
+  width: 46px;
+  height: 46px;
+}
+.car-preview {
+  width: 100%;
+  border-radius: 8px;
+}
+.checked {
+  background-color: #e4f5dd !important;
+  color: #7fc75e;
+}
+.red-button {
+  background: #d90e32;
+}
+.red-border {
+  border: 1px solid#D90E32;
+}
+.border-05 {
+  border: 0.5px solid var(--surface-300);
 }
 </style>
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="navbar">
+    <div class="flex flex-column fixed">
+      <div class="mt-4 text-center dm-sans-700 text-100 text-2xl">
+        DEMO TEST
+      </div>
+
+      <div class="mt-8 dm-sans-700">
+        <div class="flex align-items-center menu-item">
+          <img class="icon" src="../assets/images/user1.svg" alt="" />
+          <div class="pl-3 text-100">Profile</div>
+        </div>
+        <div class="flex align-items-center menu-item active">
+          <img class="icon" src="../assets/images/rim1.svg" alt="" />
+          <div class="pl-3 text-100">Vehicles</div>
+        </div>
+        <div class="flex align-items-center menu-item">
+          <img class="icon" src="../assets/images/settings1.svg" alt="" />
+          <div class="pl-3 text-100">Settings</div>
+        </div>
+      </div>
+    </div>
+    <img class="mt-auto wave-image" src="../assets/images/wave.svg" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuItems: [
+        { title: "Profile", link: "profile" },
+        { title: "Vechicles", link: "Vechicles" },
+        { title: "Settings", link: "Settings" },
+      ],
+    };
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  display: flex;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  width: 256px;
+  background-color: #282828;
+}
+.menu-item {
+  padding: 12px 30px;
+  width: 256px;
+  opacity: 0.45;
+}
+.icon {
+  width: 24px;
+  height: 25px;
+  opacity: 100 !important;
+}
+.active {
+  background-color: #535353;
+  border-left: 2px solid #d90e32;
+  opacity: 1;
+}
+.wave-image {
+  width: 256px;
+  height: 235px;
+}
+</style>
